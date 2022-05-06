4cd9d2d4778e7a6d95eb122bf250b2f836e58674
Michael Kuyper <mkuyper@mkdata.ch>
2022-05-06 11:14:26 -0400

    added missing copyright headers

--- projects/ex-fuota/fuota.c
+++ projects/ex-fuota/fuota.c
@@ -1,3 +1,8 @@
+// Copyright (C) 2020-2022 Michael Kuyper. All rights reserved.
+//
+// This file is subject to the terms and conditions defined in file 'LICENSE',
+// which is part of this source code package.
+
 // proprietary FUOTA protocol
 
 #include "lmic.h"
